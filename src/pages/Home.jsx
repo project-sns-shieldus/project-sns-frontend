e80c01e76bbfae90673c8732e3b731b7fc7844e4
Ju Yeon <129965252+ojuyeon88@users.noreply.github.com>
2024-08-26 13:09:24 +0900

    feat: 로그인 페이지 및 메인 페이지

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import HeaderContainer from "../components/HeaderContainer";
+import "../css/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <HeaderContainer />
+    </div>
+  );
+}
+
+export default Home;
