19717ec9a709c0ef554542752d5cce2f6708ac5c
vanguard <vanguard09@naver.com>
2024-08-28 23:05:18 +0900

    main 기능 구현 (팔로우된 유저의 게시물 출력) V 1.0
    게시물 작성, userid 로 유저 찾아 팔로우 기능 임시 구현. 팔로윙 유저의 게시글을 불러오는기능(이미지와 콘텐츠) 구현되어있습니다. 서비스적으로, 기능적으로 미흡한 부분 수정중입니다.

--- src/components/HeaderContainer.jsx
+++ src/components/HeaderContainer.jsx
@@ -1,13 +1,22 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import "../css/HeaderContainer.css";
 import HeaderLogo from "./HeaderLogo";
 import HeaderMenu from "./HeaderMenu";
 
 export default function HeaderContainer() {
+    // 로그인된 사용자명 가져오기
+    const username = localStorage.getItem('userId');
+
     return (
         <div className="Header">
             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}><HeaderLogo/></Link>
-            <HeaderMenu></HeaderMenu>
+            {username && (
+                <div className="Header-username">
+                    <span>안녕하세요, {username}님!</span>
+                </div>
+            )}
+            <HeaderMenu />
         </div>
     );
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import '../css/PostCard.css';
+import heartIcon from '../assets/img/Heart.svg';
+import commentIcon from '../assets/img/Edit 3.svg';
+
+import { imageApi } from '../api/controller/imageApi'; // imageApi import
+
+export default function PostCard({ post }) {
+    const [images, setImages] = useState([]); // 이미지 상태 추가
+
+    // 이미지 가져오기
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const response = await imageApi.getImagesByPostId(post.postId);
+                setImages(response.data);
+            } catch (error) {
+                console.error('이미지 로드 중 오류 발생:', error);
+            }
+        };
+
+        fetchImages();
+    }, [post.postId]); // postId가 변경될 때마다 이미지 가져오기
+
+    return (
+        <div className="post-card">
+            <div className="post-header">
+                <div className="post-author-avatar">
+                    {/* 아바타 이미지 또는 기본 아이콘 */}
+                    <img src={post.authorAvatar || '/src/assets/img/User-1.svg'} alt="Avatar" />
+                </div>
+                <h3 className="post-author-name">{post.postId}</h3>
+            </div>
+            
+            {/* 이미지 렌더링 */}
+            <div className="post-images">
+                {images.map(image => (
+                    <img 
+                        key={image.imageId} 
+                        src={`http://localhost:8080/uploads/${image.fileName}`}  // 백엔드 서버의 URL로 변경
+                        alt="Post" 
+                        className="post-image" 
+                    />
+                 ))}
+            </div>
+            {/* 공백과 줄 바꿈을 유지하며 텍스트 렌더링 */}
+            <p className="post-content" style={{ whiteSpace: 'pre-wrap' }}>
+                {post.content}
+            </p>
+
+            <div className="post-actions">
+                <button className="like-button">
+                    <img src={heartIcon} alt="Like" />
+                    좋아요
+                </button>
+                <button className="comment-button">
+                    <img src={commentIcon} alt="Comment" />
+                    댓글 달기
+                </button>
+            </div>
+        </div>
+    );
+}
